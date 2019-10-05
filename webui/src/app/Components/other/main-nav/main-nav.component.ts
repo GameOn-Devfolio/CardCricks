d90db50f088788499cd31f8a39bd648451630b65
Anas KVM <kvmanas@gmail.com>
2019-10-05 20:08:27 +0530

    logout function

--- webui/src/app/Components/other/main-nav/main-nav.component.ts
+++ webui/src/app/Components/other/main-nav/main-nav.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, DoCheck, Input } from "@angular/core";
-import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { Component, OnInit, DoCheck, Input } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 // import { Web3Service } from 'src/app/Services/web3/web3.service';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
+import { Web3Service } from 'src/app/Services/Web3/web3.service';
 
 @Component({
-  selector: "app-main-nav",
-  templateUrl: "./main-nav.component.html",
-  styleUrls: ["./main-nav.component.scss"]
+  selector: 'app-main-nav',
+  templateUrl: './main-nav.component.html',
+  styleUrls: ['./main-nav.component.scss']
 })
 export class MainNavComponent {
   @Input()
@@ -22,11 +23,12 @@ export class MainNavComponent {
   // private web3service: Web3Service,
   constructor(
     private breakpointObserver: BreakpointObserver,
-    private route: Router
+    private route: Router,
+    private web3Service: Web3Service
   ) {}
 
-  // logOut = async () => {
-  //   this.web3service.web3logout();
-  //   this.route.navigateByUrl('/Home');
-  // }
+  logOut = async () => {
+    this.web3Service.web3logout();
+    this.route.navigateByUrl('/Home');
+  };
 }
