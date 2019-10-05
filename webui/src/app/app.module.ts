0aed1bf0f351b37aec7d094ec6379abf2c69e3f5
Dennis Sam <denniarems@gmail.com>
2019-10-05 13:57:50 +0530

    game

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './Modules/material/material.module';
+import { GameComponent } from './Components/game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
