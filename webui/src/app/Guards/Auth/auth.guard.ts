862803c6001446bf1cc499c6d2bb75e454269198
Anas KVM <kvmanas@gmail.com>
2019-10-05 19:38:00 +0530

    Auth Guard

--- webui/src/app/Guards/Auth/auth.guard.ts
+++ webui/src/app/Guards/Auth/auth.guard.ts
@@ -1,22 +1,19 @@
-import { Injectable } from "@angular/core";
-import { CanActivate, Router } from "@angular/router";
-import { map } from "rxjs/operators";
-import { Observable } from "rxjs";
-import { Web3Service } from "src/app/Services/Web3/web3.service";
-// import { Web3Service } from '../Services/Web3/web3.service';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+// Guard to check is user loggin or not
+// if user is not logged redirect to /Home
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private web3service: Web3Service, private route: Router) {}
-  canActivate(): Observable<boolean> {
-    return this.web3service.Web3Details$.pipe(
-      map((n): boolean => {
-        if (n.account) {
-          return true;
-        }
-        this.route.navigateByUrl("/Home");
-      })
-    );
+  constructor(private route: Router) {}
+  canActivate(): boolean {
+    // get isLogged variable from local storage
+    const isLogged = localStorage.getItem('isLogged');
+    if (isLogged === 'true') {
+      return true;
+    }
+    this.route.navigateByUrl('/Home');
   }
 }
